2c935b0293758420245b19c141cca911f81270a4
Nick Lopez <nlopez@gmail.com>
2014-07-18 05:54:14 -0400

    warbler ready

--- Gemfile
+++ Gemfile
@@ -1,2 +1,6 @@
 source 'https://rubygems.org'
 gem 'rubydns'
+group :development do
+  gem 'rubocop'
+  gem 'warbler'
+end
---
+++ lib/dashdns.rb
@@ -0,0 +1,38 @@
+# Encoding: utf-8
+require 'rubydns'
+require 'ipaddr'
+
+class Dashdns
+  INTERFACES = [
+      [:udp, '127.0.0.1', 5300],
+      [:tcp, '127.0.0.1', 5300]
+  ]
+
+  Name = Resolv::DNS::Name
+  IN = Resolv::DNS::Resource::IN
+
+  def initialize(domain)
+    @domain = domain
+  end
+
+  def run
+    domain = @domain
+    RubyDNS.run_server(listen: INTERFACES) do
+      match(/^((\d+)-){3}(\d+)\.#{domain}$/, IN::A) do |transaction, match_data|
+        match = match_data.to_s.chomp(".#{domain}").gsub('-', '.')
+        begin
+          ip = IPAddr.new(match)
+          transaction.respond!(ip.to_s, :ttl => 86400)
+        rescue StandardError => e
+          transaction.fail!(:NXDomain)
+          logger.error(e.message)
+          logger.debug(match_data.string)
+        end
+      end
+
+      otherwise do |transaction|
+        transaction.fail!(:NXDomain)
+      end
+    end
+  end
+end
